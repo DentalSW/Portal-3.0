3c731586966ff8f009be41f3687be77b602cf8b8
peak169368 <peak169368@126.com>
2016-04-14 11:08:43 +0800

    Repository initialize

---
+++ Portal/src/main.tsx
@@ -0,0 +1,19 @@
+/// <reference path="../typings/tsd.d.ts" />
+import * as React from 'react';
+import * as DOM from 'react-dom';
+
+const root = document.getElementById('app');
+
+class Main extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+DOM.render(<Main />, root);
---
+++ Portal/src/test2.ts
@@ -0,0 +1,9 @@
+var testInt: number;  
+testInt = 5;
+
+var a:string;
+a += 1;
+
+
+var test: string = "hello world 1111111111";  
+console.log(test);  
